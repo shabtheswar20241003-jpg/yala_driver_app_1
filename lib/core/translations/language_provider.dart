25f242a98ae16d23e22e7d3fd1968b9624c45657
shabtheswar20241003-jpg <shabtheswar.20241003@iit.ac.lk>
2026-03-21 09:43:25 +0530

    lanpoverror

--- lib/core/translations/language_provider.dart
+++ lib/core/translations/language_provider.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
-import 'app_translations.dart';
 
-class LanguageProvider extends ChangeNotifier {
-  String _lang = 'en';
-
-  String get lang => _lang;
-
-  void changeLanguage(String langCode) {
-    _lang = langCode;
-    AppTranslations.currentLang = langCode;
-    notifyListeners();
-  }
-}

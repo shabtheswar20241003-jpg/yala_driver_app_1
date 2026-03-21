015bc9675bb095d92ecdb365a788dd176d7b5808
shabtheswar20241003-jpg <shabtheswar.20241003@iit.ac.lk>
2026-03-21 09:53:57 +0530

    baseadded

--- lib/core/services/supabase_client.dart
+++ lib/core/services/supabase_client.dart
@@ -1,2 +1,13 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+class SupabaseConfig {
+  static const String supabaseUrl = "https://entzwknecnwcbspqcbuk.supabase.co/";
+  static const String supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVudHp3a25lY253Y2JzcHFjYnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MTA5MDUsImV4cCI6MjA4OTA4NjkwNX0.96EH2JKnFsT5XfxNFMX-RfbJByOKSy-yT0sCEZsBP6g";
+
+  static Future<void> init() async {
+    await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  }
+
+  static SupabaseClient get client => Supabase.instance.client;
 }
